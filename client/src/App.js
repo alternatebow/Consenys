b46aa7fe972482cf2a241d3a7fb1eb27d5f7db8b
sensor47 <calslee2011@gmail.com>
2021-12-29 20:53:04 -0800

    Front End functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react"
-import { Container, Row, Col, DropdownMenu, Dropdown, DropdownToggle, DropdownItem } from 'reactstrap';
-import { render } from '@testing-library/react';
+import dice1 from "./Dice_Images/dice-six-faces-one.png";
+import dice2 from "./Dice_Images/dice-six-faces-two.png";
+import dice3 from "./Dice_Images/dice-six-faces-three.png";
+import dice4 from "./Dice_Images/dice-six-faces-four.png";
+import dice5 from "./Dice_Images/dice-six-faces-five.png";
+import dice6 from "./Dice_Images/dice-six-faces-six.png";
+
+import { Container, Row, Col, DropdownMenu, Dropdown, DropdownToggle, DropdownItem, Button } from 'reactstrap';
 import { GAMBLE_ABI, GAMBLE_ADDRESS } from './config';
+import Web3 from 'web3';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
+    //this.handleAnte = this.handleAnte.bind(this);
     this.state = {
+      account: "",
+      contractInstance: "",
+      diceValue1: dice1,
+      diceValue2: dice2,
       dropdownOpen: false,
-      betType: "Type of Bet"
+      betType: "Type of Bet",
+      betAmount: "",
+      point: "None",
+      betPlaced: false,
+      correctNetwork: false,
+      roundOver: false
     };
+
   }
 
   componentDidMount() {
+    //  window.addEventListener('load', function() {
+    //    if(window.ethereum) {
+    //      this.loadBlockchainData();
+    //    }
+    // })
+    this.loadBlockchainData();
   }
 
   async loadBlockchainData() {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const networkType = await web3.eth.net.getNetworkType();
+    console.log(networkType)
+    if(networkType !== 'kovan'){
+      alert('Must be on the kovan network!')
+    } else {
+      this.setState({correctNetwork: true});
+    }
+    /* Only uses the first account */
     const accounts = await web3.eth.getAccounts();
-    const gambleInstance = new web3.eth.Contract(GAMBLE_ABI, GAMBLE_ADDRESS);
-    
+    this.setState({
+      account: accounts[0],
+      contractInstance: new web3.eth.Contract(GAMBLE_ABI, GAMBLE_ADDRESS)
+    });
   }
 
   toggle() {
@@ -31,19 +65,38 @@ export default class App extends React.Component {
       dropdownOpen: !prevState.dropdownOpen
     }));
   }
+  
+  async playGame(betType, betValue) {
+    let bet = (betType == "Pass") ? 0 : 1;
+    // Subscribe to all events from the contract
+    this.state.contractInstance.events.BetPlaced(() => {
+      this.setState({betPlaced: true});
+      console.log(this.state.betPlaced)
+    });
+    this.state.contractInstance.events.DiceResults(function(error, event){console.log("Hello World")})
+    this.state.contractInstance.events.Winner(() => {
+      this.setState({betPlace: false})
+    });
+    this.state.contractInstance.events.Loser(() => {
+        this.setState({betPlace: false})
+      });
+    this.state.contractInstance.events.Point(function(error, event){console.log("Hello World")});
+    this.state.contractInstance.methods.createGame(bet).send({from: this.state.account, value: betValue});
+    
+  }
+    
+    
 
   render() {
    return (
-    <div className="App">
+    <div className="App" style={{
+      visibility: this.state.correctNetwork ? 'visible' : 'hidden'
+    }}>
+      Craps Game
           <Container>
           <Row 
-            md="4"
-            sm="2"
-            xs="1"
             className='dice-header' 
           >
-            <Col>
-            </Col>
             <Col
             >
               <img src={this.state.diceValue1} className="dice" alt="logo" />
@@ -52,16 +105,16 @@ export default class App extends React.Component {
             >
               <img src={this.state.diceValue2} className="dice" alt="logo" />
             </Col>
-            <Col>
-            </Col>
           </Row>
+          <br></br>
           <Row
             className='display'
           >
             <Col>
+            <Button>Connect Wallet</Button>
             </Col>
             <Col>
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} disabled={this.state.betPlaced}>
                 <DropdownToggle caret>
                   {this.state.betType}
                 </DropdownToggle>
@@ -71,24 +124,34 @@ export default class App extends React.Component {
                 </DropdownMenu>
               </Dropdown>
             </Col>
+            <br></br>
             <Col  xs='auto'>
-              <form>
+              <form >
                 <label>
-                  Bet Amount:
-                  <input type="text"/>
-                  <input type="submit" value="Bet" />
-                </label>
+                  Bet Amount <br></br>in wei <br></br> (min bet is 10000000000000)
+                  </label>
+                  <input type="text" value={this.state.betAmount} disabled={this.state.betPlaced} onChange={(e) => {
+                    this.setState({betAmount: e.target.value})
+                  }}></input>
               </form>
             </Col>
             <Col>
-              <button>Roll!</button>
+              <button onClick={
+                () => {
+                  if(this.state.betAmount < 10000000000000 && (this.state.betType != 'Pass' || this.state.betType != 'No Pass')) {
+                    alert("Bet amount is too low!")
+                  } else{
+                    this.playGame(this.state.betType, this.state.betAmount);
+                  } 
+                }
+                }>Roll!</button>
             </Col>
           </Row>
           <Row>
-            Bet Type:
+            Bet Type: {this.state.betType}
           </Row>
           <Row>
-            Bet Amount:
+            Bet Amount: {this.state.betAmount}
           </Row>
           <Row>
             Point:
